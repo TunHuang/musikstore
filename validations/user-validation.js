15e1e4a722ce583ee58b66d53a1d19028ab89d2e
TunHuang <huang.tun.anton@gmail.com>
2021-02-15 15:55:28 +0100

    Validierungsarrays aus users.js in validations/user-validation.js ausgelagert

---
+++ validations/user-validation.js
@@ -0,0 +1,74 @@
+const { check } = require('express-validator');
+
+const validDataUser = [
+  check('vorname')
+    .not()
+    .isEmpty()
+    .withMessage('Vorname muss angegeben werden.')
+    .isAlpha('de-DE', { ignore: ' -' })
+    .withMessage('Vorname enthält unzulässige Zeichen')
+    .trim(),
+  check('nachname')
+    .not()
+    .isEmpty()
+    .withMessage('Nachname muss angegeben werden.')
+    .isAlpha('de-DE', {ignore: ' -'})
+    .withMessage('Nachname enthält unzulässige Zeichen')
+    .trim(),
+  check('email')
+    .not()
+    .isEmpty()
+    .withMessage('E-Mail muss angegeben werden.')
+    .isEmail()
+    .withMessage('E-Mail-Format ist ungültig.')
+    .trim()
+    .normalizeEmail(),
+  check('password')
+    .not()
+    .isEmpty()
+    .withMessage('Passwort muss angegeben werden.')
+    .isStrongPassword()
+    .withMessage('Passwort ist nicht sicher. Es soll mindestens acht Zeichen enthalten, davon mindestens eine Kleinbuchstabe, mindestens eine Großbuchstabe, mindestens eine Nummer und mindestens ein Sonderzeichen.')
+    .trim()
+];
+
+const validDataUpdateUser = [
+  check('vorname')
+    .not()
+    .isEmpty()
+    .optional()
+    .withMessage('Vorname muss angegeben werden.')
+    .isAlpha('de-DE', { ignore: ' -' })
+    .withMessage('Vorname enthält unzulässige Zeichen')
+    .trim(),
+  check('nachname')
+    .not()
+    .isEmpty()
+    .optional()
+    .withMessage('Nachname muss angegeben werden.')
+    .isAlpha('de-DE', {ignore: ' -'})
+    .withMessage('Nachname enthält unzulässige Zeichen')
+    .trim(),
+  check('email')
+    .not()
+    .isEmpty()
+    .optional()
+    .withMessage('E-Mail muss angegeben werden.')
+    .isEmail()
+    .withMessage('E-Mail-Format ist ungültig.')
+    .trim()
+    .normalizeEmail(),
+  check('password')
+    .not()
+    .isEmpty()
+    .optional()
+    .withMessage('Passwort muss angegeben werden.')
+    .isStrongPassword()
+    .withMessage('Passwort ist nicht sicher. Es soll mindestens acht Zeichen enthalten, davon mindestens eine Kleinbuchstabe, mindestens eine Großbuchstabe, mindestens eine Nummer und mindestens ein Sonderzeichen.')
+    .trim()
+];
+
+module.exports = {
+  validDataUser,
+  validDataUpdateUser
+};
